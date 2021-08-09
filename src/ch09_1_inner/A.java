c50b8ae8d3853b71e0e41b9ad26dc2675d0161f6
bticamp <bticamp@DESKTOP-H0IJBRQ>
2021-08-09 18:02:14 +0900

    210809

---
+++ src/ch09_1_inner/A.java
@@ -0,0 +1,47 @@
+package ch09_1_inner;
+
+public class A {
+	
+	/**¹Ù±ù Å¬·¡½º**/
+	A() {
+		System.out.println("A °´Ã¼°¡ »ý¼ºµÊ"); }
+
+		
+		/**ÀÎ½ºÅÏ½º ¸â¹ö Å¬·¡½º**/
+		class B {
+			B() { System.out.println("B °´Ã¼°¡ »ý¼ºµÊ"); }
+				int field1;
+				//static int field2;
+				void method1() {}
+				//static void method2() {}
+				
+			}
+			
+			/** Á¤Àû ¸â¹ö Å¬·¡½º**/
+			static class C {
+				C() { System.out.println("C °´Ã¼°¡ »ý¼ºµÊ"); }
+					int field1;
+					static int field2;
+					void method1() {}
+					static void method2() {}
+					
+				}
+				
+				void method() {
+					/**·ÎÄÃ Å¬·¡½º**/
+					class D {
+						D() { System.out.println("D °´Ã¼°¡ »ý¼ºµÊ"); }
+							int field1;
+							//static int field2;
+							void method1() {}
+							//static void method2() {}
+						}
+						D d = new D();
+						d.field1 = 3;
+						d.method1();
+
+
+	}
+	
+
+}
